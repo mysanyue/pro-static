7c9f0ba68887c4d08a25915d3ef5be2c69fcd9f0
张伟 <myzony@qq.com>
2021-01-20 17:40:11 +0800

    cursor

---
+++ assets/cursor.js
@@ -0,0 +1,29 @@
+$(function() {
+  const target = '.common-grid-container.move>canvas'
+  // 网格
+  $('body').on('mousemove.grid', target, openhand)
+  $('body').on('mouseover.grid', target, openhand)
+  $('body').on('mouseout.grid', target, cursorDefault)
+  $('body').on('mouseup.grid', target, function() {
+    $('body').on('mousemove.grid', target, openhand)
+    openhand.call($(this))
+  })
+  $('body').on('mousedown.grid', target, function(event) {
+    $('body').off('mousemove.grid')
+    closedhand.call($(this), event)
+  })
+
+  function openhand() {
+    $(this).css('cursor', 'url(/assets/images/openhand.cur), default')
+  }
+
+  function closedhand(event) {
+    if (event.button == 0) {
+      $(this).css('cursor', 'url(/assets/images/closedhand.cur), move')
+    }
+  }
+
+  function cursorDefault() {
+    $(this).css('cursor', 'default')
+  }
+});
