ccc2c83925c2ace12cc191058d20f4afea03af2f
三月风 <myzony@qq.com>
2022-10-08 22:37:33 +0800

    feat: contact

--- wechart-shop/pages/contact/contact.js
+++ wechart-shop/pages/contact/contact.js
@@ -1,11 +1,13 @@
 Page({
   data: {
     colorList: [],
+    isLoading: false
   },
   onLoad() {
     this.getColors()
   },
   onReachBottom() {
+    if (this.data.isLoading) return
     this.getColors()
   },
 
@@ -13,11 +15,19 @@ Page({
    * @description 随机获取颜色
    */
   getColors() {
+    this.setData({ isLoading: true })
+    wx.showLoading({ title: '数据加载中...' })
     wx.request({
       url: 'https://www.escook.cn/api/color',
       method: 'GET',
       success: ({ data: res }) => {
-        this.setData({ colorList: [...this.data.colorList, ...res.data] })
+        this.setData({
+          colorList: [...this.data.colorList, ...res.data],
+        })
+      },
+      complete: () => {
+        wx.hideLoading()
+        this.setData({ isLoading: false })
       },
     })
   },
